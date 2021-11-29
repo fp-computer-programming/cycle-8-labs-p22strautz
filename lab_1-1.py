7e63ea07304f82f9a31f58969b17e1156acc39da
p22strautz <p22strautz@fairfieldprep.org>
2021-11-29 11:00:07 -0500

    lab 1-1, 1-2

---
+++ lab_1-1.py
@@ -0,0 +1,9 @@
+# Author: SCT (AMDG) 11/29/21
+
+def greeting():
+    '''Prints \"Hello World!\"'''
+    print("Hello World!")
+
+
+help(greeting)
+greeting()
---
+++ lab_1-2.py
@@ -0,0 +1,9 @@
+# Author: SCT (AMDG) 11/29/21
+
+def find_sum(num1, num2):
+    num_sum = num1 + num2
+    return num_sum
+
+
+my_sum = find_sum(111, 222)
+print(my_sum)
