5d714d7046b846526f863de1a15d1eccd57a46a5
p22strautz <p22strautz@fairfieldprep.org>
2021-12-01 10:21:17 -0500

    finish lab 2-1,2-2

---
+++ lab_2-1.py
@@ -0,0 +1,11 @@
+# Author: SCT (AMDG) 12/1/21
+
+def sum_to(num):
+    total = 0
+    for value in range(int(num+1):
+        total += value
+
+
+
+number = input("Input a number:")
+print(sum_to(number))
---
+++ lab_2-2.py
@@ -0,0 +1,8 @@
+# Author: SCT (AMDG) 12/1/21
+
+def party_invites(lst):
+    for name in lst:
+        print("Hi {0}, You're invited to my party".format(name))
+
+
+party_invites(["Micheal", "John", "Steve"])
