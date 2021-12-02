9edaf178fed00ba31828768b987792a4436c35bf
p22strautz <p22strautz@fairfieldprep.org>
2021-12-02 09:36:07 -0500

    lab 2-1, 2-2

--- lab_2-1.py
+++ lab_2-1.py
@@ -1,11 +1,9 @@
 # Author: SCT (AMDG) 12/1/21
-
-def sum_to(num):
+def sum(num):
     total = 0
-    for value in range(int(num+1):
+    for value in range(int(num)+1):
         total += value
 
 
-
 number = input("Input a number:")
-print(sum_to(number))
+sum(sum(number))
